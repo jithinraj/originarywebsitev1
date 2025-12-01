3195a8b72b0262b477e92bb1b64f5ed0edd458bd
jithinraj <jithinraj@users.noreply.github.com>
2025-12-02 04:56:13 +0530

    seo

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,21 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: '*', allow: '/' }],
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Bingbot',
+        allow: '/',
+      },
+    ],
     sitemap: 'https://www.originary.xyz/sitemap.xml',
+    host: 'https://www.originary.xyz',
   }
 }
