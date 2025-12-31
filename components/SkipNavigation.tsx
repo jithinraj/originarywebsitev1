464586abd1e0b987164ad0cfad5cfbbfc80cc463
jithinraj <jithinraj@users.noreply.github.com>
2025-12-31 08:38:30 +0530

    fix skip navigation visibility

--- components/SkipNavigation.tsx
+++ components/SkipNavigation.tsx
@@ -5,7 +5,6 @@ export default function SkipNavigation() {
     <a
       href="#main-content"
       className="skip-nav"
-      onFocus={(e) => e.target.scrollIntoView({ behavior: 'smooth', block: 'center' })}
     >
       Skip to main content
     </a>
